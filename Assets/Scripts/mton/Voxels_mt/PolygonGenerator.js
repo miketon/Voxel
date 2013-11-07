b1e5cd1cc870ff964e5290955cac3fe0c61dc0a1
Mike Ton <mike.ton@gmail.com>
2013-11-06 19:24:25 -0800

    Dance Quad.  Dance.

--- Assets/Scripts/mton/Voxels_mt/PolygonGenerator.js
+++ Assets/Scripts/mton/Voxels_mt/PolygonGenerator.js
@@ -1,7 +1,8 @@
 ﻿#pragma strict
 
-public var nVertices:Vector2[];
-public var nTriangles:int[];
+private var arrVertices:Array = new Array(); 
+private var arrTriangles:Array = new Array(); 
+private var arrUV:Array = new Array(); 
 
 private var mesh:Mesh;
 private var xform:Transform;
@@ -18,13 +19,11 @@ function Start () {
   var y:float = xform.position.y;
   var z:float = xform.position.z;
 
-  var arrVertices:Array = new Array();   //positive x is to the right and negative y is down
   arrVertices.Push( Vector3 (x  , y  , z ));
   arrVertices.Push( Vector3 (x + 1 , y  , z ));
   arrVertices.Push( Vector3 (x + 1 , y-1  , z ));
   arrVertices.Push( Vector3 (x  , y-1  , z ));
   
-  var arrTriangles:Array = new Array();
   //with Unity mesh class you create your triangles by adding the three points clockwise, 
   //this defines which direction is solid on your mesh
   arrTriangles.Push(0);
@@ -34,7 +33,6 @@ function Start () {
   arrTriangles.Push(2);
   arrTriangles.Push(3);
   
-  var arrUV:Array = new Array();  
   //Unity's texture's 0,0 and 0,0 is the bottom left...WTH:Wish texture 0,0 were top left :(
   //To match vertice order : define the top left, then the top right, then the bottom right and last the bottom left.
   //To create the UV:defines what 4 corners of the texture you would like the four corners of the square to use
@@ -43,6 +41,21 @@ function Start () {
   arrUV.Push(Vector2(tUnit * tStone.x + tUnit, tUnit * tStone.y));
   arrUV.Push(Vector2(tUnit * tStone.x, tUnit * tStone.y));
 
+}
+
+function Update () {
+  var magnitude :float = 0.95;
+  var magTime : float = 2.5;
+  var randIndex = Random.Range(0.0, 3.0);
+  var vecTemp:Vector3 = arrVertices[randIndex];
+  vecTemp.x = Mathf.Sin(Time.time* randIndex *magTime)*magnitude;
+  vecTemp.y = Mathf.Cos(Time.time* randIndex *magTime)*magnitude;
+  arrVertices[randIndex] = vecTemp;
+  print(arrVertices[randIndex]);
+  UpdateMesh();
+}
+
+function UpdateMesh(){
   mesh.Clear ();
   mesh.vertices  = arrVertices.ToBuiltin(Vector3) as Vector3[];
   mesh.uv = arrUV.ToBuiltin(Vector2) as Vector2[];
@@ -50,17 +63,3 @@ function Start () {
   mesh.Optimize ();           //Optimizes the mesh for display?? Does no harm; doesn't spend cycles unnecessarily
   mesh.RecalculateNormals (); //Recalculates the normals of the mesh from the triangles and vertices.
 }
-
-/*
-function Update () {
-  // Get instantiated mesh
-  var mesh : Mesh = GetComponent(MeshFilter).mesh;
-  // Randomly change vertices
-  var vertices : Vector3[] = mesh.vertices;
-  for (var p : int = 0; p < vertices.Length; p++) {
-    vertices[p] += Vector3(0, Random.Range(-0.3, 0.3), 0);
-  }
-  mesh.vertices = vertices;
-  mesh.RecalculateNormals();
-}
-*/
